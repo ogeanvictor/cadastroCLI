09f4ecc1662dc21bcefbacd23015b6cb0d9b742e
Gean Victor <geanvictorduque@gmail.com>
2024-05-02 19:22:38 -0300

    feat: Create file read function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/utils/questions.txt");
+
+        Scanner sc = new Scanner(file);
+
+        while(sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
+
     }
 }
